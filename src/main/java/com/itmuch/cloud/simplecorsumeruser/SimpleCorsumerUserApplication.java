edb57b9b45ba082deda02edba6f86e2a7574f6d2
vnsun <vn_sun@163.com>
2020-08-29 17:33:30 +0800

    提交

---
+++ src/main/java/com/itmuch/cloud/simplecorsumeruser/SimpleCorsumerUserApplication.java
@@ -0,0 +1,13 @@
+package com.itmuch.cloud.simplecorsumeruser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleCorsumerUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleCorsumerUserApplication.class, args);
+	}
+
+}
